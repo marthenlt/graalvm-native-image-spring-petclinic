f9ecae7da05e52e7a6cb06dfaa2a64fe5e010fb7
Marthen Luther <marthenlt@gmail.com>
2020-10-18 21:59:20 +0800

    fix mysql 8.0 connection string that caused NPE due to  Authentication plugin 'caching_sha2_password' cannot be loaded: /usr/lib64/mysql/plugin/caching_sha2_password.so: cannot open shared object file: No such file or directory

--- src/main/resources/db/mysql/user.sql
+++ src/main/resources/db/mysql/user.sql
@@ -7,4 +7,14 @@ ALTER DATABASE petclinic
 --GRANT ALL PRIVILEGES ON petclinic.* TO 'petclinic@%' IDENTIFIED BY 'petclinic';
 
 create user 'petclinic'@'%' identified by 'petclinic';
+
+-- For MySQL 8.0, to avoid NPE
+-- java.sql.SQLNonTransientConnectionException: Could not create connection to database server.
+--      at com.mysql.cj.jdbc.exceptions.SQLError.createSQLException(SQLError.java:110) ~[na:na]
+-- due to :
+--      Authentication plugin 'caching_sha2_password' cannot be loaded: /usr/lib64/mysql/plugin/caching_sha2_password.so: cannot open shared object file: No such file or directory
+-- uncomment below :
+ALTER USER 'petclinic'@'%' IDENTIFIED WITH mysql_native_password BY 'petclinic';
+
 grant all on petclinic.* to 'petclinic'@'%';
+FLUSH PRIVILEGES;
