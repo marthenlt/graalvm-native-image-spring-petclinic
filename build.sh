c4e7d034d5c9062b2440bf3f49dd9e432980fde5
Marthen Luther <marthenlt@gmail.com>
2020-10-28 13:49:59 +0800

    delete redundant build script (build.sh), in favour of compile.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-BLUE='\033[0;34m'
-NC='\033[0m'
-
-printf "=== ${BLUE}Building %s sample${NC} ===\n" "${PWD##*/}"
-./compile.sh
-
-#docker build -t marthenl/petclinic-mysql-native-image:0.8 .
