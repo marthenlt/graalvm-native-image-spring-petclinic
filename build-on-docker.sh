7c80bcf247e82a520020b43065c1205c7f885e9f
Marthen Luther <marthenlt@gmail.com>
2020-11-12 01:11:51 +0800

    fix some layout issue in the template, enhance the script to strip html tags, increase the version

--- build-on-docker.sh
+++ build-on-docker.sh
@@ -2,4 +2,4 @@
 
 ./mvnw package -DskipTests
 
-docker build -f Dockerfile-multistage -t marthenl/petclinic-mysql-native-image:0.11 .
+docker build -f Dockerfile-multistage -t marthenl/petclinic-mysql-native-image:0.12 .
--- check-who-is-serving.sh
+++ check-who-is-serving.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-for i in {1..2000}; do curl -s http://petclinic-knative-serving.default.127.0.0.1.xip.io/doscale | grep served && sleep 0.3; done
+for i in {1..5000}; do curl -s http://petclinic-knative-serving.default.127.0.0.1.xip.io/doscale | grep served | sed -e 's/<[^>]*>//g' && sleep 0.2; done
